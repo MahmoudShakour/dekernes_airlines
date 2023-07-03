5f78cace96cd677dbe5e1e902cf0bf30f572b7d9
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-07-03 21:09:11 +0300

    Edit filterflight component

--- client/src/Components/FlightFilter.js
+++ client/src/Components/FlightFilter.js
@@ -1,56 +1,45 @@
 import filterFlight from "../Api/filterFlight";
 import style from "../Styles/FlightFilter.module.css";
+import location from "../Assets/location.svg";
+import date from "../Assets/date.svg";
 
-export default function FlightFilter({setFlights}) {
+export default function FlightFilter({ setFlights }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data={
+    const data = {
       from: e.target.from.value,
       to: e.target.to.value,
       beginDate: e.target.startdate.value,
       endDate: e.target.enddate.value,
       type: e.target.flight_type.value,
     };
-    const flights=await filterFlight(data);
+    const flights = await filterFlight(data);
     console.log(flights);
-    if(flights===false){
+    if (flights === false) {
       console.log("error");
-    }
-    else{
+    } else {
       setFlights([...flights]);
     }
   };
 
   return (
-    <form
-      className={style.flightForm}
-      action=""
-      method="POST"
-      onSubmit={handleSubmit}
-    >
-      <div>Filter</div>
-      <input type="text" id="from" placeholder="from" name="from" />
-      <input type="text" id="to" placeholder="to" name="to" />
-      <input
-        type="date"
-        id="startdate"
-        placeholder="startdate"
-        name="startdate"
-      />
-      <input type="date" id="enddate" placeholder="enddate" name="enddate" />
-      <label>
-        <input type="radio" id="domestic" name="flight_type" value="domestic" />
-        domestic
-      </label>
-      <label>
-        <input
-          type="radio"
-          id="international"
-          name="flight_type"
-          value="international"
-        />
-        international
-      </label>
+    <form className={style.flightForm} method="POST" onSubmit={handleSubmit}>
+      <div className={style.container}>
+        <img className={style.location} src={location} required alt="" />
+        <input type="text" id="from" placeholder="from*" name="from" />
+      </div>
+      <div className={style.container}>
+        <img className={style.location} src={location} required alt="" />
+        <input type="text" id="to" placeholder="to*" name="to" />
+      </div>
+      <div className={style.container}>
+        <img className={style.date} src={date} required alt="" />
+        <input type="text" id="startdate" placeholder="start date*" name="startdate" />
+      </div>
+      <div className={style.container}>
+        <img className={style.date} src={date} required alt="" />
+        <input type="text" id="enddate" placeholder="end date*" name="enddate" />
+      </div>
       <button type="submit">Filter</button>
     </form>
   );
